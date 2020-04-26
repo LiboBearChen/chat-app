8306f5028209207cf2274a5c69934da5a0ab7c88
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2020-04-26 14:09:33 -0400

    update

--- server/index.js
+++ server/index.js
@@ -30,13 +30,17 @@ io.on('connection', (socket)=>{
 
     socket.on('sendMessage', (message, callback)=>{
         const user=getUser(socket.id);
-        io.to(user.room).emit('message', {user: user.name, text: message});
+        io.to(user.room).emit('message', {user: user.name, text: message})
 
         callback();
     });
 
     socket.on('disconnect', ()=>{
-        console.log('User had left!!!');  
+        const user=removeUser(socket.id);
+        
+        if(user){
+            io.to(user.room).emit('message', {user:'admin', text:`${user.name} has left.`})
+        }
     })
 });
 
